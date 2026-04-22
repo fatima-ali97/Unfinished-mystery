bf89c6af826a37adfa10a300b81b7b1576ab5c7a
202304651 <202304651@student.polytechnic.bh>
2026-04-22 22:15:13 +0300

    save as a scene

--- Unfinished-mystery/Assets/Scripts/UI/InkDrawing/UIDrawer.cs
+++ Unfinished-mystery/Assets/Scripts/UI/InkDrawing/UIDrawer.cs
@@ -4,48 +4,56 @@ using UnityEngine.EventSystems;
 public class UIDrawer : MonoBehaviour, IPointerDownHandler, IDragHandler
 {
     public GameObject inkPrefab;
-    public Transform drawingParent;
-
-    // How close the dots are (smaller = smoother but more lag
+    public RectTransform drawingArea;  
     public float dotSpacing = 2.0f;
     private Vector2 lastPointerPosition;
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        lastPointerPosition = eventData.position;
-        CreateDot(eventData.position);
+        // Only draw if the click is actually inside the box
+        if (IsInsideBoundary(eventData.position))
+        {
+            lastPointerPosition = eventData.position;
+            CreateDot(eventData.position);
+        }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        float distance = Vector2.Distance(lastPointerPosition, eventData.position);
-
-        // fill the gape between mouse positions if the distance is greater than the spacing
-        if (distance > dotSpacing)
+        // Only draw the next dots if the mouse is still inside the box
+        if (IsInsideBoundary(eventData.position))
         {
-            int dotsToSpawn = Mathf.FloorToInt(distance / dotSpacing);
+            float distance = Vector2.Distance(lastPointerPosition, eventData.position);
 
-            for (int i = 1; i <= dotsToSpawn; i++)
+            if (distance > dotSpacing)
             {
-                // last pos + current pos * t
-                float t = (float)i / dotsToSpawn;
-                Vector2 interpolatedPosition = Vector2.Lerp(lastPointerPosition, eventData.position, t);
-                CreateDot(interpolatedPosition);
+                int dotsToSpawn = Mathf.FloorToInt(distance / dotSpacing);
+                for (int i = 1; i <= dotsToSpawn; i++)
+                {
+                    float t = (float)i / dotsToSpawn;
+                    Vector2 interpolatedPosition = Vector2.Lerp(lastPointerPosition, eventData.position, t);
+                    CreateDot(interpolatedPosition);
+                }
             }
+            lastPointerPosition = eventData.position;
         }
+    }
 
-        lastPointerPosition = eventData.position;
+    
+    bool IsInsideBoundary(Vector2 screenPos)
+    {
+        return RectTransformUtility.RectangleContainsScreenPoint(drawingArea, screenPos, null);
     }
 
     void CreateDot(Vector2 position)
     {
-        GameObject newDot = Instantiate(inkPrefab, drawingParent);
+        GameObject newDot = Instantiate(inkPrefab, drawingArea);
         newDot.transform.position = position;
     }
 
     public void ClearDrawing()
     {
-        foreach (Transform child in drawingParent)
+        foreach (Transform child in drawingArea)
         {
             Destroy(child.gameObject);
         }
