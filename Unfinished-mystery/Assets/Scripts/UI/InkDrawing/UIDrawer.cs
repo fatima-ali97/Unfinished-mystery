f58a33e0657b74befc3022f3fdd3035b615b03c4
202304651 <202304651@student.polytechnic.bh>
2026-04-22 20:32:20 +0300

    smoothen the ink drawing mechanic

--- Unfinished-mystery/Assets/Scripts/UI/InkDrawing/UIDrawer.cs
+++ Unfinished-mystery/Assets/Scripts/UI/InkDrawing/UIDrawer.cs
@@ -1,20 +1,40 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
-using System.Collections.Generic;
 
 public class UIDrawer : MonoBehaviour, IPointerDownHandler, IDragHandler
 {
-    public GameObject inkPrefab; 
-    public Transform drawingParent; 
+    public GameObject inkPrefab;
+    public Transform drawingParent;
+
+    // How close the dots are (smaller = smoother but more lag
+    public float dotSpacing = 2.0f;
+    private Vector2 lastPointerPosition;
 
     public void OnPointerDown(PointerEventData eventData)
     {
+        lastPointerPosition = eventData.position;
         CreateDot(eventData.position);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        CreateDot(eventData.position);
+        float distance = Vector2.Distance(lastPointerPosition, eventData.position);
+
+        // fill the gape between mouse positions if the distance is greater than the spacing
+        if (distance > dotSpacing)
+        {
+            int dotsToSpawn = Mathf.FloorToInt(distance / dotSpacing);
+
+            for (int i = 1; i <= dotsToSpawn; i++)
+            {
+                // last pos + current pos * t
+                float t = (float)i / dotsToSpawn;
+                Vector2 interpolatedPosition = Vector2.Lerp(lastPointerPosition, eventData.position, t);
+                CreateDot(interpolatedPosition);
+            }
+        }
+
+        lastPointerPosition = eventData.position;
     }
 
     void CreateDot(Vector2 position)
@@ -23,7 +43,6 @@ public class UIDrawer : MonoBehaviour, IPointerDownHandler, IDragHandler
         newDot.transform.position = position;
     }
 
-    // Add a button to call this to clear the page!
     public void ClearDrawing()
     {
         foreach (Transform child in drawingParent)
